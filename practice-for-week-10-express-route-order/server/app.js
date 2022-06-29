4b619bf7d1503113685fb400f958dbdd9de7af5b
Alex Hiller <asonnyhiller@gmail.com>
2022-06-28 23:55:30 -0400

    huh

--- practice-for-week-10-express-route-order/server/app.js
+++ practice-for-week-10-express-route-order/server/app.js
@@ -5,20 +5,20 @@ app.get('/', (req, res) => {
     res.send("Server is alive");
 });
 
-app.get('/hello', (req, res) => {
-    res.send("Hello, my friend!");
-});
-
-app.get(['/goodbye', '/goodbye/*'], (req, res) => {
-    res.send("Goodbye, my friend!");
+app.get('/goodbye/until/forever', (req, res) => {
+    res.send("So long. Farewell. Have a great life!");
 });
 
 app.get('/goodbye/until/:time', (req, res) => {
     res.send(`Goodbye. See you ${req.params.time}.`);
 });
 
-app.get('/goodbye/until/forever', (req, res) => {
-    res.send("So long. Farewell. Have a great life!");
+app.get('/hello', (req, res) => {
+    res.send("Hello, my friend!");
+});
+
+app.get(['/goodbye', '/goodbye/*'], (req, res) => {
+    res.send("Goodbye, my friend!");
 });
 
 const port = 5000;
